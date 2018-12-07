d5d1d86540c93857ebc00d33d31ca9f6ec1de226
Codey <codeymcdaniel@gmail.com>
2018-12-06 18:54:14 -0700

    removed console logs

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -22,7 +22,6 @@ $(document).ready(function() {
     gifSearch(topic);
   });
 });
-
 function displayTopics() {
   $("#topics").empty();
   for (let i = 0; i < topics.length; i++) {
@@ -33,7 +32,6 @@ function displayTopics() {
     $("#topics").append(button);
   }
 }
-
 function gifSearch(topic) {
   gifArray = [];
   $("#results").empty();
@@ -51,8 +49,6 @@ function gifSearch(topic) {
         "<h1>There was a problem connecting to the server</h1>"
       );
     } else {
-      console.log("Data Object:", data);
-      // console.log("Status: ", status);
       for (let i = 0; i <= data.data.length - 1; i++) {
         gifArray.push({
           stillImage: data.data[i].images.original_still.url,
@@ -68,23 +64,17 @@ function gifSearch(topic) {
             "<div class='card-body p-0 bg-dark text-white'>" +
             "<p class='card-text py-2 pl-1'>Rating: " +
             gifArray[i].rating.toUpperCase() +
-            //"<a href='" +
-            //gifArray[i].gif +
-            //"' download>" +
             "<i class='fas fa-download float-right mr-2 mt-1 text-white'></i>" +
-            //"</a>" +
             "</p>" +
             "</div>" +
             "</div>"
         );
       }
     }
-    console.log(gifArray);
     playGif();
     downloadGif();
   });
 }
-
 function playGif() {
   $("img").on("click", function() {
     for (let i = 0; i < gifArray.length; i++) {
@@ -96,7 +86,6 @@ function playGif() {
     }
   });
 }
-
 function downloadGif() {
   $("i").on("click", function() {
     for (let i = 0; i < gifArray.length; i++) {
